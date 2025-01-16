0b357f0fbbe1a9293d29452df368f28cfeb36178
Sharmukhan <sharmukhan@gmail.com>
2025-01-16 16:04:26 +0500

    Initial Commit

---
+++ AddContactsTests/AddContactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AddContactsTests.swift
+//  AddContactsTests
+//
+//  Created by Kymbat Sharmukhan on 16.01.2025.
+//
+
+import Testing
+@testable import AddContacts
+
+struct AddContactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
